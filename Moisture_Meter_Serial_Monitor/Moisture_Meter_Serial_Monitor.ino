d8bc71e1a08d89b1b9e12e3a76a8cc6f81e9a6c7
Jeffrey Beckman <JeffreyJBeckman@gmail.com>
2015-08-02 16:54:41 -0700

    Add in LED functionality under a moisture limit.

--- Moisture_Meter_Serial_Monitor/Moisture_Meter_Serial_Monitor.ino
+++ Moisture_Meter_Serial_Monitor/Moisture_Meter_Serial_Monitor.ino
@@ -1,8 +1,10 @@
 int moistureSensor = 0;   //one nail goes to +5V, the other nail goes to this analogue pin
 int moisture_val;
+int led = 13;
 
 void setup() {
   Serial.begin(9600);     //open serial port
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
@@ -10,4 +12,13 @@ void loop() {
   Serial.print("moisture sensor reads ");
   Serial.println( moisture_val );     // print the moisture level to the serial monitor
   delay(500);
+  if (moisture_val < 800)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+  }
+  
 }
